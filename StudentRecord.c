8b0f700ccd2fea7e27374c50ac02bba02f99b4b9
nguyenthanhnam2901 <127769365+nguyenthanhnam2901@users.noreply.github.com>
2023-04-27 17:25:20 +0700

    Update StudentRecord.c

--- StudentRecord.c
+++ StudentRecord.c
@@ -3,118 +3,133 @@
 #include <string.h>
 #include <stdbool.h>
 #include "student.h"
+#include "Check.h"
 
 int n;
 student students[1003];
+int mx(int a, int b) {
+    return (a > b ? a : b);
+}
 
-void input() {
+void input()
+{
     bool numberstudent = false;
-    while(!numberstudent)
+    while (!numberstudent)
     {
         printf("Enter the number of students: ");
         printf("\n");
         scanf("%d", &n);
-        if(n<0 || n>1000) 
+        if (!isValidNumberOfStudents(n))
         {
             printf("Invalid number of students. Please try again\n");
         }
+        
         else
         {
             numberstudent = true;
         }
-    }
 
-    for(int i = 1; i <= n; ++i) {
-        bool validID = false;
-        while(!validID) {
-            printf("Enter the information of student #%d\n", i);
-            printf("ID: ");
-            scanf("%s", students[i].id);
-            printf("\n");
-            // Check if the ID has already been entered
-            bool duplicateID = false;
-            for (int j = 1; j < i; ++j) {
-                if (strcmp(students[j].id, students[i].id) == 0) {
-                    duplicateID = true;
-                    break;
+        for (int i = 1; i <= n; ++i)
+        {
+            bool validID = false;
+            while (!validID)
+            {
+                printf("Enter the information of student #%d\n", i);
+                printf("ID: ");
+                scanf("%s", students[i].id);
+                printf("\n");
+
+                if (isDuplicateID(students, i, students[i].id))
+                {
+                    printf("Duplicate ID. Please enter a different ID.\n");
+                }
+
+                else
+                {
+                    validID = true;
                 }
             }
-            if (duplicateID) {
-                printf("Duplicate ID. Please enter a different ID.\n");
-            } else {
-                validID = true;
-            }
-        }
 
-        printf("Fullname, last name and first name, separated by a space: ");
-        scanf("%s%s", students[i].fullName.lastName, students[i].fullName.firstName);
-        printf("\n");
-
-        // Validate birthdate input
-        bool validBirthdate = false;
-        while (!validBirthdate) {
-            printf("Birthdate in the format date month year: ");
-            scanf("%d%d%d", &students[i].birthDay.day, &students[i].birthDay.month, &students[i].birthDay.year);
+            printf("Fullname, last name and first name, separated by a space: ");
+            scanf("%s%s", students[i].fullName.lastName, students[i].fullName.firstName);
             printf("\n");
-            if (students[i].birthDay.day < 1 || students[i].birthDay.day > 31 || 
-                students[i].birthDay.month < 1 || students[i].birthDay.month > 12 || 
-                students[i].birthDay.year < 1) {
-                printf("Invalid birthdate. Please try again.\n");
-            } else {
-                validBirthdate = true;
-            }
-        }
 
-        // Validate grade input
-        bool validGrade = false;
-        while (!validGrade) {
-            printf("Algebra, Calculus, BP grade, separated by a space: ");
-            scanf("%f%f%f", &students[i].grade[0], &students[i].grade[1], &students[i].grade[2]);
-            printf("\n");
-            if (students[i].grade[0] < 0 || students[i].grade[0] > 20 || 
-                students[i].grade[1] < 0 || students[i].grade[1] > 20 || 
-                students[i].grade[2] < 0 || students[i].grade[2] > 20) {
-                printf("Invalid grade. Please try again.\n");
-            } else {
-                validGrade = true;
+            // Validate birthdate input
+            bool validBirthdate = false;
+            while (!validBirthdate)
+            {   
+                printf("Birthdate in the format date month year: ");
+                scanf("%d%d%d", &students[i].birthDay.day, &students[i].birthDay.month, &students[i].birthDay.year);
+                printf("\n");
+                if (!isValidBirthdate(students[i].birthDay.day, students[i].birthDay.month, students[i].birthDay.year))
+                {
+                    printf("Invalid birthdate. Please try again.\n");
+                }
+                
+                else
+                {
+                    validBirthdate = true;
+                }
+            }
+
+            // Validate grade input
+            bool validGrade = false;
+            while (!validGrade)
+            {
+                printf("Algebra, Calculus, BP grade, separated by a space: ");
+                scanf("%f%f%f", &students[i].grade[0], &students[i].grade[1], &students[i].grade[2]);
+                printf("\n");
+                if (!isValidGrade(students[i].grade))
+                {
+                    printf("Invalid grade. Please try again.\n");
+                }
+
+                else
+                {
+                    validGrade = true;
+                }
             }
         }
     }
 }
 
+int tableLen[7] = {2,9,9,7,8,17,4};
 
-void printToFile() {
-    FILE* fp = fopen("studentList.txt", "w");
-    fprintf(fp, "ID\tFullname\tBirthdate\tAlgebra\tCalculus\tBP\tGPA\n");
-    for (int i = 1; i <= n; ++i) {
-        fprintf(fp, "%s\t%s %s\t%d/%d/%d\t%.2f\t%.2f\t%.2f\t%.2f\n", 
-            students[i].id, 
-            students[i].fullName.lastName, students[i].fullName.firstName,
-            students[i].birthDay.day, students[i].birthDay.month, students[i].birthDay.year,
-            students[i].grade[0], students[i].grade[1], students[i].grade[2], 
-            gpa(students[i])
-        );
-    }
-    fclose(fp);
-}
-
-int main() {
+int main()
+{
     input();
-
-    /*
-    printf("ID\tFullname\tBirthdate\tAlgebra\tCalculus\tBP\tGPA\n");
     for(int i = 1; i <= n; ++i) {
-        printf("%s\t%s %s\t%d/%d/%d\t%.2f\t%.2f\t%.2f\t%.2f\n", 
-            students[i].id, 
-            students[i].fullName.lastName, students[i].fullName.firstName,
-            students[i].birthDay.day, students[i].birthDay.month, students[i].birthDay.year,
-            students[i].grade[0], students[i].grade[1], students[i].grade[2], 
-            gpa(students[i])
-        );
+        tableLen[0] = mx(tableLen[0],strlen(students[i].id));
+        tableLen[1] = mx(tableLen[1],strlen(students[i].fullName.lastName) + strlen(students[i].fullName.firstName) + 1);
+        tableLen[2] = mx(tableLen[2], numLen(students[i].birthDay.day) + numLen(students[i].birthDay.month) + numLen(students[i].birthDay.year) + 2);
     }
-    */
-   
-    printToFile();
-
-    return 0;
+    printf("ID");
+    for(int i = 1; i <= tableLen[0] - 2; ++i) printf(" ");
+    printf("\tFull Name"); 
+    for(int i = 1; i <= tableLen[1] - 9; ++i) printf(" ");
+    printf("\tBirthdate");
+    for(int i = 1; i <= tableLen[2] - 9; ++i) printf(" ");
+    printf("\tAlgebra       ");
+    printf("Calculus       ");
+    printf("Basic Programming       ");
+    printf("GPA\n");
+    for(int i = 1; i <= n; ++i) {
+        printf("%s", students[i].id);
+        for(int j = 1; j <= tableLen[0] - strlen(students[i].id); ++j) printf(" ");
+        printf("\t%s %s", students[i].fullName.lastName, students[i].fullName.firstName);
+        for(int j = 1; j <= tableLen[1] - strlen(students[i].fullName.lastName) - strlen(students[i].fullName.firstName); ++j) printf(" ");
+        printf("\t%d/%d/%d", students[i].birthDay.day, students[i].birthDay.month, students[i].birthDay.year);
+        for(int j = 1; j <= tableLen[2] - (numLen(students[i].birthDay.day) + numLen(students[i].birthDay.month) + numLen(students[i].birthDay.year) + 2); ++j) printf(" ");
+        for(int j = 0; j <= 2; ++j) {
+            if(students[i].grade[j] < 10.0) {
+                printf("       %.2f", students[i].grade[j]);
+                for(int k = 1; k <= tableLen[3 + j] - 4; ++k) printf(" ");
+            }
+            else {
+                printf("       %.2f", students[i].grade[j]);
+                for(int k = 1; k <= tableLen[3 + j] - 5; ++k) printf(" ");
+            }
+        }
+        printf("       %.2f\n",gpa(students[i]));
+    }
 }
